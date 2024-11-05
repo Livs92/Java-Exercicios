b728c68d2fdc632d79ca9e43d0d6027af20c2be3
Lívia Silveira da Rosa <149737602+Livs92@users.noreply.github.com>
2024-11-05 19:03:32 -0300

    Update CalculadoraDeITBI.java

--- CalculadoraDeITBI.java
+++ CalculadoraDeITBI.java
@@ -1,23 +1,14 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class CalculadoraDeITBI {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite o valor de transação do imóvel: ");
-        double valorTransacao = scanner.nextDouble();
-
-        System.out.print("Digite o valor venal do imóvel: ");
-        double valorVenal = scanner.nextDouble();
-
-        System.out.print("Digite a porcentagem de ITBI: ");
-        double porcentagemITBI = scanner.nextDouble();
+        double valorTransacao = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de transação do imóvel:"));
+        double valorVenal = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor venal do imóvel:"));
+        double porcentagemITBI = Double.parseDouble(JOptionPane.showInputDialog("Digite a porcentagem de ITBI:"));
 
         double valorBase = Math.max(valorTransacao, valorVenal);
         double impostoITBI = valorBase * (porcentagemITBI / 100);
 
-        System.out.printf("O valor do ITBI é: %.2f\n", impostoITBI);
-        
-        scanner.close();
+        JOptionPane.showMessageDialog(null, String.format("O valor do ITBI é: %.2f", impostoITBI));
     }
-}
+
