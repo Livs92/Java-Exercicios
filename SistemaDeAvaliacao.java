5d9202f08e12067911f4bea8e3df23c562e8ca6e
Lívia Silveira da Rosa <149737602+Livs92@users.noreply.github.com>
2024-11-05 19:04:03 -0300

    Update SistemaDeAvaliacao.java

--- SistemaDeAvaliacao.java
+++ SistemaDeAvaliacao.java
@@ -1,23 +1,14 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class SistemaDeAvaliacao {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite a nota da primeira prova: ");
-        double notaProva1 = scanner.nextDouble();
-
-        System.out.print("Digite a nota da segunda prova: ");
-        double notaProva2 = scanner.nextDouble();
-
-        System.out.print("Digite a nota do trabalho: ");
-        double notaTrabalho = scanner.nextDouble();
+        double notaProva1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota da primeira prova:"));
+        double notaProva2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota da segunda prova:"));
+        double notaTrabalho = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota do trabalho:"));
 
         double media = (notaProva1 + notaProva2 + notaTrabalho) / 3;
 
         String status = (media >= 6) ? "Aprovado" : "Reprovado";
-        System.out.printf("Média: %.2f - Status: %s\n", media, status);
-
-        scanner.close();
+        JOptionPane.showMessageDialog(null, String.format("Média: %.2f - Status: %s", media, status));
     }
 }
